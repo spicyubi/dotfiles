2d1149699b2c995ad97b8f1ac60b90541d0c5cd2
kwura <kevin.wurangian@gmail.com>
2024-03-21 15:41:54 +0800

    Enable tailwind lsp and enable border for cmp

--- nvim/init.lua
+++ nvim/init.lua
@@ -472,6 +472,7 @@ require('lazy').setup {
         pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+        tailwindcss = {},
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
@@ -599,6 +600,16 @@ require('lazy').setup {
 
       -- cmp setup
       cmp.setup {
+        -- window config
+        window = {
+          completion = {
+            border = 'rounded',
+          },
+          documentation = {
+            border = 'rounded',
+          },
+        },
+        -- specify snippet engine (luasnip)
         snippet = {
           expand = function(args)
             luasnip.lsp_expand(args.body)
