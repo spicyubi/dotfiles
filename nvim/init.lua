938ee992e0b00d768b3908721921a34d8ca03ae8
kwura <kevin.wurangian@gmail.com>
2024-03-30 15:55:58 +0800

    Added search mechanism change

--- nvim/init.lua
+++ nvim/init.lua
@@ -292,7 +292,7 @@ require('lazy').setup {
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      -- vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
@@ -320,12 +320,12 @@ require('lazy').setup {
       end, { desc = '[S]earch [/] in Open Files' })
 
       -- Custom Options for find files
-      vim.keymap.set('n', '<leader>sf', function()
+      vim.keymap.set('n', '<leader>sp', function()
         builtin.find_files {
           hidden = true,
           search_dirs = { '~/code/' },
         }
-      end, { desc = '[S]earch [F]iles' })
+      end, { desc = '[S]earch [P]rojects' })
 
       -- Shortcut for searching your neovim configuration files
       vim.keymap.set('n', '<leader>sn', function()
