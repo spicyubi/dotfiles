f1e064d15c4e2d134523a01c72290dff512d59a7
Kevin Wurangian <kevin.wurangian@pm.me>
2026-03-03 14:57:28 +0700

    alacritty and nvim

--- linux/nvim/lua/options.lua
+++ linux/nvim/lua/options.lua
@@ -30,6 +30,14 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>')
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
+vim.keymap.set('n', '<leader>lr', ':Leet run<Enter>')
+vim.keymap.set('n', '<leader>ls', ':Leet submit<Enter>')
+vim.keymap.set('n', '<leader>ll', ':Leet lang<Enter>')
+vim.keymap.set('n', '<leader>lf', ':Leet list<Enter>')
+vim.keymap.set('n', '<leader>lt', ':Leet tabs<Enter>')
+vim.keymap.set('n', '<leader>le', ':Leet exit<Enter>')
+vim.keymap.set('n', '<leader>lo', ':Leet open<Enter>')
+vim.keymap.set('n', '<leader>ld', ':Leet desc<Enter>')
 
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
@@ -40,3 +48,4 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 vim.cmd('set completeopt+=noselect')
+vim.cmd('set nolist')
--- linux/nvim/lua/plugins.lua
+++ linux/nvim/lua/plugins.lua
@@ -1,29 +1,44 @@
 vim.pack.add({
-	{src='https://github.com/folke/tokyonight.nvim'},
+	{src='https://github.com/rebelot/kanagawa.nvim'},
 	{src='https://github.com/NMAC427/guess-indent.nvim'},
-	{src='https://github.com/nvim-treesitter/nvim-treesitter',version='main'},
+	{src='https://github.com/nvim-treesitter/nvim-treesitter', version = 'master'},
 	{src='https://github.com/folke/snacks.nvim'},
 	{src='https://github.com/nvim-lua/plenary.nvim'},
 	{src='https://github.com/MunifTanjim/nui.nvim'},
 	{src='https://github.com/kawre/leetcode.nvim'},
 })
 
-require('tokyonight').setup({
-	styles = {
-		comments = {italic = false}
+-- https://github.com/rebelot/kanagawa.nvim/issues/216
+require('kanagawa').setup({
+	commentStyle = {italic=false},
+	keywordStyle = {italic=false},
+	statementStyle = {bold=false},
+	overrides = function()
+	    return {
+	      ["@variable.builtin"] = { italic = false },
+	    }
+	  end,
+	transparent = false,
+	theme = 'dragon',
+	background = {
+		dark = 'dragon',
+		light = 'lotus'
 	},
 })
-vim.cmd.colorscheme('tokyonight-night')
-vim.cmd(':hi statusline guibg=None')
 
-require('nvim-treesitter.config').setup({
-	ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+vim.cmd.colorscheme('kanagawa')
+vim.cmd(':hi statusline guibg=None')
+vim.cmd(':hi SignColumn guibg=None')
+vim.cmd(':hi LineNr guibg=None')
+
+require('nvim-treesitter.configs').setup({
+	ensure_installed = { 'python', 'bash', 'c','lua', 'luadoc', 'vim', 'vimdoc', 'html' },
 	auto_install = false,
 	highlight = {enable = true,additional_vim_regex_highlighting=false},
-	indent = { enable = true} 
+	indent = { enable = true},
 })
 -- todo: use autocommand callback
-vim.cmd(':TSUpdate')
+-- vim.cmd(':TSUpdate')
 
 require('snacks').setup({
     picker = { enabled = true },
